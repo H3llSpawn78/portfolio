1e294777fb1d5517a84e34bf3eeccd43396d744e
Rich M <rmery32@hotmail.com>
2025-04-12 15:02:09 +0100

    initial meta data added to default layout

--- nuxtfolio/app/layouts/default.vue
+++ nuxtfolio/app/layouts/default.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
+    {{ settings }}
     <slot />
   </div>
 </template>
+
+<script lang="ts" setup>
+const prismic = usePrismic();
+
+const { data: settings } = await useAsyncData(() =>
+  prismic.client.getSingle("settings")
+);
+
+useSeoMeta({
+  title: settings.value?.data.site_title,
+  ogTitle: settings.value?.data.site_title,
+  description: settings.value?.data.meta_description,
+  ogDescription: settings.value?.data.meta_description,
+  ogImage: computed(() => prismic.asImageSrc(settings.value?.data.meta_image)),
+});
+</script>
+
+<style></style>
